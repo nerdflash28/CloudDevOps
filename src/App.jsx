e3eb69996b07c2940f0dfa989cf9f808b2bef48d
The-Phoenics <kar9205an@gmail.com>
2024-11-21 00:49:26 +0530

    project restructure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-900">
+      This is todo project!
+    </h1>
+  )
+}
+
+export default App
